0eaf2a85c4d5a5f58e87e4abc8704aa093d95de6
Ray Rainier <rayrainier@yahoo.com>
2017-01-18 14:35:47 -0600

    LoginController: UIViewController

---
+++ gameofchats/LoginController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginController.swift
+//  gameofchats
+//
+//  Created by RayRainier on 1/18/17.
+//  Copyright © 2017 RadiuSense. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor(red: 61/255, green: 91/255, blue: 151/255, alpha: 1)
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+
+
+}
