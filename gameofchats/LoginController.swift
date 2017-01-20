f40cb1aeafbfd608c41bf028a4a859f08817d7b6
Ray Rainier <rayrainier@yahoo.com>
2017-01-19 21:05:28 -0600

    LoginController: UIViewController ... setupInputsContainerView() constraints...

--- gameofchats/LoginController.swift
+++ gameofchats/LoginController.swift
@@ -10,15 +10,42 @@ import UIKit
 
 class LoginController: UIViewController {
 
+    let inputsContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.white
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = UIColor(red: 61/255, green: 91/255, blue: 151/255, alpha: 1)
+        
+        view.backgroundColor = UIColor(r: 61, g: 91, b: 151)
+        
+        view.addSubview(inputsContainerView)
+		
+        setupInputsContainerView()
+        
+    }
+    
+	func setupInputsContainerView() {
+        // x,y,width, height constraints
+        inputsContainerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        inputsContainerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        inputsContainerView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
+        inputsContainerView.heightAnchor.constraint(equalToConstant: 150).isActive = true
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
 
+}
 
+extension UIColor {
+    
+    convenience init(r: CGFloat, g: CGFloat, b: CGFloat) {
+        self.init(red: r/255, green: g/255, blue: b/255, alpha: 1)
+    }
 }
