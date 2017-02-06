a5960c7b74baeb83639729887d7953a7187b5288
Ray Rainier <rayrainier@yahoo.com>
2017-02-06 16:35:03 -0600

    let bubbleView: UIView = {
            let view = UIView()
            view.backgroundColor = UIColor(r: 0, g: 137, b: 249)
            view.translatesAutoresizingMaskIntoConstraints = false
            return view

--- gameofchats/ChatMessageCell.swift
+++ gameofchats/ChatMessageCell.swift
@@ -16,13 +16,30 @@ class ChatMessageCell: UICollectionViewCell {
         tv.text = "SAMPLE TEXT FOR NOW"
         tv.font = UIFont.systemFont(ofSize: 16)
         tv.translatesAutoresizingMaskIntoConstraints = false
+        tv.backgroundColor = UIColor.clear
         return tv
+    }()
+    
+    let bubbleView: UIView = {
+        
+        let view = UIView()
+        view.backgroundColor = UIColor(r: 0, g: 137, b: 249)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
+        addSubview(bubbleView)
         addSubview(textView)
+        
+        // add x,y,w,h iOS9 constraints
+        bubbleView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        bubbleView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        bubbleView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        bubbleView.heightAnchor.constraint(equalTo: self.heightAnchor).isActive = true
+        
         // add x,y,w,h iOS9 constraints
         textView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
         textView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
